aad06a6b7490faec1c72365bd75a4d8f183d1448
Quderlo <4546436@gmail.com>
2020-05-28 15:19:43 +0300

    add ex6

--- ex5.py
+++ ex5.py
@@ -1,3 +1,8 @@
+#Задание
+#Создать список из словарей с оценками учеников разных классов школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
+#Посчитать и вывести средний балл по всей школе.
+#Посчитать и вывести средний балл по каждому классу.
+
 import random
 
 def marks():
@@ -6,14 +11,14 @@ def marks():
 		mark.append(random.randint(2,5))
 	return mark
 
-def fun(tmp):
+def fun_sum(tmp):
 	x = 0
 	for j in tmp:
 		x += j
 	return x
 
 def average_class(tmp):
-	return str((fun(tmp) / len(tmp)))
+	return str((fun_sum(tmp) / len(tmp)))
 
 def average_school(sum, total_lenght):
 	return str(sum / total_lenght)
@@ -27,13 +32,7 @@ for i in letter:
 		tmp = marks()
 		roster.append({'school_class': str(z) + i, 'scores': tmp})
 		print('Средний балл в классе ' + str(z) + i + ' равен ' + average_class(tmp))
-		sum += fun(tmp)
+		sum += fun_sum(tmp)
 		total_lenght += len(tmp)
 
 print('Средний балл по школе равен ' + average_school(sum, total_lenght))
-
-
-#Задание
-#Создать список из словарей с оценками учеников разных классов школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
-#Посчитать и вывести средний балл по всей школе.
-#Посчитать и вывести средний балл по каждому классу.
